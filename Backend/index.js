86ed8dd58dc61dd32764df2ac4a1b1d09872d81d
Durgesh kumar <Kumardurgeshjha398@gmail.com>
2025-08-29 00:24:48 +0530

    first commit

--- Backend/index.js
+++ Backend/index.js
@@ -28,10 +28,10 @@ app.use("/api/chat", chatRouter)
 
 const __dirname = path.resolve()
 
-app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.use(express.static(path.join(__dirname, "/Frontend/dist")));
 
 app.get(/^\/(?!api).*/,(req,res) =>{
-    res.sendFile(path.join(__dirname, "frontend","dist","index.html"));
+    res.sendFile(path.join(__dirname, "Frontend","dist","index.html"));
 })
 
 app.listen(port, ()=>{
